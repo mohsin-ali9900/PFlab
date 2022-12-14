6363af0ff968286a8ca06312fcbe1308b186e9cf
Office <mohsin99alii@gmail.com>
2022-12-14 15:48:36 +0500

    pf lab 2 tasks

---
+++ projectheader.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main(){
+cout <<"****************************************" <<endl ;
+cout <<"*                                      *" <<endl;
+cout <<"*                                      *" <<endl;
+cout <<"*       MOHSIN SOTWARE HOUSE           *" <<endl;
+cout <<"*                                      *" <<endl;
+cout <<"*                                      *" <<endl;
+cout <<"*                                      *" <<endl;
+cout <<"*                                      *" <<endl;
+cout <<"****************************************" <<endl ;
+}
