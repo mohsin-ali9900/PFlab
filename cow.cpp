af658537215123355a5dd5283cc6502e52ec5bf1
mohsin-ali9900 <120159587+mohsin-ali9900@users.noreply.github.com>
2022-12-19 08:00:49 -0800

    Add files via upload

---
+++ cow.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout << "----------------------------" << endl;
+cout << endl;
+cout << "       o   ^   ^             " << endl;
+cout << "            ---             " << endl;
+cout << "        o  (oo) \---------- " << endl;
+cout << "           (--) \         )\/\ " << endl;
+cout << "                ||------w | " << endl;
+cout << "                ||       || ";
+}
---
+++ headerproject.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout << "******************************************************" << endl;
+cout << "*                                                    *" << endl;
+cout << "*                                                    *" << endl;
+cout << "*                                                    *" << endl;
+cout << "*            University Admission                    *" << endl;
+cout << "*             Mangement System                       *" << endl;
+cout << "*                                                    *" << endl;
+cout << "*                                                    *" << endl;
+cout << "*                                                    *" << endl;
+cout << "******************************************************";
+}
